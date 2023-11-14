b646eed9c6e7b0d01f0aba8489430d0a5a26e215
fifthfir <fifthfir2017@gmail.com>
2023-11-14 10:37:45 -0800

    lab2

---
+++ disc/disc02/Fib.java
@@ -0,0 +1,9 @@
+public class Fib {
+    public static int fib(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }else {
+            return fib(n - 2) + fib(n - 1);
+        }
+    }
+}
