ae2f6f1db733f67ae91d9f2521933654a90f112d
fifthfir <fifthfir2017@gmail.com>
2023-12-01 13:05:44 -0800

    lab4b

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -2,18 +2,21 @@
  *  @author Rain Zhang
  */
 public class Collatz {
-    /** next number if 1 return 0 */
+
+    /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n%2 == 0) {
-            return n/2;
-        } else {
-            return n * 3 + 1;
+        int ret = 0;
+        if (n % 2 == 0) {
+            ret = n / 2;
+        }else if (n % 2 == 1 && n != 1) {
+            ret = 3 * n + 1;
         }
+        return ret;
     }
 
     public static void main(String[] args) {
         int n = 5;
-        while (n != 1) {
+        while (n != 0) {
             System.out.print(n);
             System.out.print(' ');
             n = nextNumber(n);
